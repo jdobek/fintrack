4b13fed2b4058f1f4bd1ac36fad38136eb040f11
Jakub Dobek <jakubdobek@gmail.com>
2025-10-17 22:56:59 +0200

    Dashboard view

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { cn } from "@/lib/utils"
 import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
+import './globals.css';
 
 export const metadata: Metadata = {
   title: {
@@ -42,10 +43,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
         >
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <div className="relative flex min-h-screen flex-col">
-              <SiteHeader />
               <div className="flex-1">{children}</div>
             </div>
-            <TailwindIndicator />
+            
           </ThemeProvider>
         </body>
       </html>
