a5cdd5d131b61d0ae3f9fcc48e71d278e4d155fb
Jakub Dobek <jakubdobek@gmail.com>
2025-10-24 23:08:36 +0200

    site-header fixed & button add expenses added

--- app/expenses/page.tsx
+++ app/expenses/page.tsx
@@ -1,6 +1,9 @@
 import { AppSidebar } from "@/components/app-sidebar"
 import { SiteHeader } from "@/components/site-header"
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
+import { Button } from "@/components/ui/button"
+import { Plus } from "lucide-react"
+
 
 export default function ExpensesPage() {
   return (
@@ -11,12 +14,14 @@ export default function ExpensesPage() {
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-              <div className="px-4 lg:px-6">
+
+              <div className="px-4 lg:px-6 flex items-center justify-between">
                 <h1 className="text-2xl font-bold">Expenses</h1>
-                <p className="text-muted-foreground">
-                  Track and manage your expenses
-                </p>
+                <Button variant="default">
+                <Plus/> Add Expense 
+                  </Button>
               </div>
+
               <div className="px-4 lg:px-6">
                 <div className="rounded-lg border border-dashed p-8 text-center">
                   <p className="text-muted-foreground">
